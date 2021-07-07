71394220667847024a367d402940e50327967fa8
Quinn0823 <3547347506@qq.com>
2021-07-07 11:50:42 +0800

    Update line.js

--- js/line.js
+++ js/line.js
@@ -1,5 +1,8 @@
 function line() {
         document.title = document.getElementById("line").value + " | CQPT";
-        if(document.getElementById("line").value == 224) {
+        if(document.getElementById("line").value == 224p) {
+
+        } else if(document.getElementById("line").value == 224o) {
+
         }
 }
